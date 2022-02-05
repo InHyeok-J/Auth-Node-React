c18468f5bed8bc1f45f144a1f8fb98c0de0ba678
inhyeok <ss112d86@gmail.com>
2022-02-05 20:56:03 +0900

    Prisma Setting

--- nestback/src/app.module.ts
+++ nestback/src/app.module.ts
@@ -4,13 +4,20 @@ import { UsersModule } from './users/users.module';
 import { EmailService } from './email/email.service';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { PrismaModule } from 'nestjs-prisma';
+import { loggingMiddleware } from './common/middleware/prisma.logger.middleware';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
       load: [emailConfig],
       isGlobal: true,
+    }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [loggingMiddleware()],
+      },
     }),
     UsersModule,
     EmailModule,
--- nestback/src/main.ts
+++ nestback/src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   await app.listen(3000);
 }
 bootstrap();
