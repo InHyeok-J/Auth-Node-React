b259c4dcfd80bfbeff86623506bdb23da885a065
inhyeok <ss112d86@gmail.com>
2022-02-11 23:41:16 +0900

    Oauth Test init

---
+++ proxyOAuth/back-oauth/src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import session from 'express-session';
+import morgan from 'morgan';
+import env from './config';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // 받은 데이터를 req에 넣어줌.
+app.use(cors({ origin: true, credentials: true }));
+app.use(cookieParser(env.COOKIE_SECRET));
+app.use(
+  session({
+    secret: env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.get('/', (req, res, next) => {
+  res.send('gd');
+});
+
+app.listen(env.PORT || 4000, () => {
+  console.log(env.PORT + '서버 시작');
+});
